da1fd109f864bd3cd3d87a6386463640eea2701b
nicolas PIRON <piron.nicolas@gmail.com>
2016-05-27 12:23:40 +0200

    pannellum

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,8 @@
 (function() {
-	$("#myvirtualtour").ipanorama({
-		theme: "ipnrm-theme-modern",
-		hotSpotSetup: true,
-		sceneId: "entrance",
-		scenes: {
-			entrance: {
-				type: "sphere", // specifies the scene type ("box", "sphere", "cylinder")
-				image: "assets/images/pano.jpg",
-				title: "The entrance to the house",
-			},
-		}
+
+	pannellum.viewer('myvirtualtour', {
+	    "type": "equirectangular",
+	    "panorama": "./assets/images/pano.jpg"
 	});
+
 })();
